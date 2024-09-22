baffadffceacb1dd0bd71ce55ce0f9a26491e279
Joao Reis <joaoteireis@gmail.com>
2024-09-21 22:00:36 -0300

    OrderItem, many-to-many association with extra attributes

---
+++ src/main/java/com/webservice/springService/entities/pk/OrderItemPk.java
@@ -0,0 +1,51 @@
+package com.webservice.springService.entities.pk;
+
+import com.webservice.springService.entities.Order;
+import com.webservice.springService.entities.Product;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class OrderItemPk implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        OrderItemPk that = (OrderItemPk) object;
+        return Objects.equals(order, that.order) && Objects.equals(product, that.product);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(order, product);
+    }
+}
